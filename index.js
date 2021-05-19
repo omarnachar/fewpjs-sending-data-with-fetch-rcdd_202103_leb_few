e83b6c8ba0c431de443c7947d53bf79ea3e60150
Omar Nachar <omarnachar2@gmail.com>
2021-05-19 13:47:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
 // Add your code here
+{
+  "dogs": [
+    {
+      "dogName": "Luca",
+      "dogBreed": "German Shepherd",
+      "id": 1
+	@@ -8,9 +9,31 @@
+      "dogName": "Lola",
+      "dogBreed": "Giant Schnauzer",
+      "id": 2
+    },
+    {
+      "dogName": "Colby",
+      "dogBreed": "Bull Mastiff",
+      "submit": "Submit",
+      "id": 3
+    },
+    {
+      "dogName": "Byron",
+      "dogBreed": "Poodle",
+      "id": 4
+    },
+    {
+      "dogName": "Byron",
+      "dogBreed": "Poodle",
+      "id": 5
+    },
+    {
+      "dogName": "Byron",
+      "dogBreed": "Poodle",
+      "id": 6
+    }
+  ],
+  "cats": [
+    {
+      "catName": "Stanley",
+      "catBreed": "Tabby",
+      "id": 1
+	@@ -21,10 +44,33 @@
+      "id": 2
+    }
+  ],
+  "users": [
+    {
+      "name": "nate",
+      "email": "natemail",
+      "id": 1
+    },
+    {
+      "name": "jeff",
+      "email": "jeffmail",
+      "id": 2
+    },
+    {
+      "name": "jeff",
+      "email": "jeffmail",
+      "id": 3
+    },
+    {
+      "name": "nate",
+      "email": 2,
+      "id": 4
+    }
+  ],
+  "robots": [
+    {
+      "robotName": "C8-09S100K",
+      "robotType": "Human Assistant",
+      "id": 9000
+    }
+  ]
