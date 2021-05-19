8bbf76c3825764a03167d3c28ff1893f12420667
Omar Nachar <omarnachar2@gmail.com>
2021-05-19 13:49:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,72 +1,33 @@
 // Add your code here
-{
-  "dogs": [
-    {
-      "dogName": "Luca",
-      "dogBreed": "German Shepherd",
-      "id": 1
-	@@ -8,9 +9,31 @@
-      "dogName": "Lola",
-      "dogBreed": "Giant Schnauzer",
-      "id": 2
-    },
-    {
-      "dogName": "Colby",
-      "dogBreed": "Bull Mastiff",
-      "submit": "Submit",
-      "id": 3
-    },
-    {
-      "dogName": "Byron",
-      "dogBreed": "Poodle",
-      "id": 4
-    },
-    {
-      "dogName": "Byron",
-      "dogBreed": "Poodle",
-      "id": 5
-    },
-    {
-      "dogName": "Byron",
-      "dogBreed": "Poodle",
-      "id": 6
-    }
-  ],
-  "cats": [
-    {
-      "catName": "Stanley",
-      "catBreed": "Tabby",
-      "id": 1
-	@@ -21,10 +44,33 @@
-      "id": 2
-    }
-  ],
-  "users": [
-    {
-      "name": "nate",
-      "email": "natemail",
-      "id": 1
-    },
-    {
-      "name": "jeff",
-      "email": "jeffmail",
-      "id": 2
-    },
-    {
-      "name": "jeff",
-      "email": "jeffmail",
-      "id": 3
-    },
-    {
-      "name": "nate",
-      "email": 2,
-      "id": 4
-    }
-  ],
-  "robots": [
-    {
-      "robotName": "C8-09S100K",
-      "robotType": "Human Assistant",
-      "id": 9000
-    }
-  ]
+function submitData(name, email) {
+    let formData = {
+        name: name,
+        email: email
+    };
+
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(object) {
+            let h2 = document.createElement('h2');
+            h2.innerHTML = object.id;
+            document.body.appendChild(h2);
+            console.log(object);
+        })
+        .catch(function(error) {
+            let h3 = document.createElement('h3');
+            h3.innerHTML = error.message;
+            document.body.appendChild(h3);
+            console.log(error.message);
+        });
+}
